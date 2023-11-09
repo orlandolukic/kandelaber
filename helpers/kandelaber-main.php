1058d11912119f97fe345f48ffcf4d9f3ec80639
Luka Orlandić <orlandolukic96@gmail.com>
2023-11-09 20:48:52 +0100

    [master] Updated versions of CSS and JS

--- helpers/kandelaber-main.php
+++ helpers/kandelaber-main.php
@@ -12,8 +12,8 @@ if ( ! class_exists('KandelaberMain') ) {
             return self::$instance;
         }
 
-        private static $CSS_VERSION = '1.0.0';
-        private static $JS_VERSION = '1.0.0';
+        private static $CSS_VERSION = '1.0.1';
+        private static $JS_VERSION = '1.0.2';
 
         private function __construct()
         {
